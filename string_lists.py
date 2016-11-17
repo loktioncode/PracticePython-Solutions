fe0c34251ba41e39efbab3804f37d08f09e346ea
Vuyisile Ndlovu <terrameijar@gmail.com>
2016-11-17 15:12:50 +0200

    Palindrome checker

--- string_lists.py
+++ string_lists.py
@@ -2,10 +2,19 @@
 Ask the user for a string and print out whether this string is a palindrome or not.
 (A palindrome is a string that reads the same forwards and backwards.)
 '''
+def is_palindrome():
 
-palindrome = "boob"
-#usr_str = raw_input("Enter a word to check ")
+    palindrome = raw_input("Enter a word to check ")
+    left = 0
+    right = len(palindrome)
 
-rev = palindrome[-1:0]
-print rev
-#for character in usr_str:
+    while left < right:
+        if palindrome[left]!= palindrome[right - 1]:
+            print "Not Palindrome"
+            quit()
+        left += 1
+        right -= 1
+    print "Palindrome"
+
+if __name__ == "__main__":
+    is_palindrome()
